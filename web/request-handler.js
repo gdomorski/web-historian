f2474f42fbf1e0799986c382577d09a31c0b0ef9
gdomorski <greg.domorski@gmail.com>
2015-12-16 16:16:29 -0800

    pass get test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,32 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var http = require('http');
+var fs = require('fs');
+
+
 // require more modules/folders here!
+var statusCode = 200;
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  fs.readFile('/Users/student/Desktop/2015-11-web-historian/web/public/index.html', function(err, html){
+    if(err){
+      throw err;
+    }
+    if (req.method === "GET") {
+      res.writeHead(statusCode, archive.headers);
+      res.write(html);
+      res.end();
+
+      if(req.url === 'google.com'){
+        sites.txt
+
+      }  
+        //check if the url is in the list
+          //if it is not, add the url to the list addUrlToList
+
+
+
+    }
+  })
+
 };
